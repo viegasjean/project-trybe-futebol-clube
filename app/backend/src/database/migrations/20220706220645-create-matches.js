ec62557c99977c36fac54ca7650a5fc782a88657
Jean Viegas <jeanmelg@gmail.com>
2022-07-06 19:24:14 -0300

    feix: change migration table name to matches

--- app/backend/src/database/migrations/20220706220645-create-matches.js
+++ app/backend/src/database/migrations/20220706220645-create-matches.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('BlogPosts', {
+    await queryInterface.createTable('matches', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -42,6 +42,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('BlogPosts');
+    await queryInterface.dropTable('matches');
   }
 };
